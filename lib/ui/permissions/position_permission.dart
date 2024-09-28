9c21821529c6b2af4800e7724db26128c05a7beb
mMatteo <matteomurino2@gmail.com>
2024-09-28 15:17:09 +0200

    Update geoLocation in case of negative response

--- lib/ui/permissions/position_permission.dart
+++ lib/ui/permissions/position_permission.dart
@@ -1,25 +1,24 @@
 import 'package:geolocator/geolocator.dart';
 
-Future<Position> determinePosition() async {
+Future<Position?> determinePosition() async {
   bool serviceEnabled;
   LocationPermission permission;
 
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
-    return Future.error('Location services are disabled.');
+    return null;
   }
 
   permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      return Future.error('Location permissions are denied');
+      return null;
     }
   }
 
   if (permission == LocationPermission.deniedForever) {
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
+    return null;
   }
 
   return await Geolocator.getCurrentPosition();
--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,14 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:mappa_indicatore/ui/controller/camera_controller.dart';
-import 'package:mappa_indicatore/ui/controller/delete_controller.dart';
 import 'package:mappa_indicatore/ui/controller/download_controller.dart';
-import 'package:mappa_indicatore/ui/controller/upload_controller.dart';
 import 'package:mappa_indicatore/ui/models/custom_marker.dart';
 import 'package:mappa_indicatore/ui/providers/login_provider.dart';
 import 'package:mappa_indicatore/ui/utility/dialog_image.dart';
@@ -26,7 +20,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<CustomMarker> listaMarkers = [];
   late GoogleMapController mapController;
-  late Future<Position>
+  late Future<Position?>
       _localizationFuture; // Salvo la variabile riempita per non dover ogni volta ricaricare la funzione
 
   void _onMapCreated(GoogleMapController controller) {
@@ -41,7 +35,7 @@ class _HomePageState extends State<HomePage> {
     loadMarkers();
   }
 
-  Future<Position> getLocalization() async {
+  Future<Position?> getLocalization() async {
     return await determinePosition();
   }
 
@@ -86,98 +80,103 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: FutureBuilder<Position>(
+      body: FutureBuilder<Position?>(
         future: _localizationFuture,
         builder: (context, snapshot) {
+          LatLng posizioneGeo;
+
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (snapshot.hasData) {
             localizationProvider.geolocalization = snapshot.data;
-            final LatLng posizioneGeo = LatLng(
+            posizioneGeo = LatLng(
               localizationProvider.geolocalization!.latitude,
               localizationProvider.geolocalization!.longitude,
             );
-
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.grey.shade200,
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: GoogleMap(
-                        onMapCreated: _onMapCreated,
-                        onTap: loginProvider.token.isNotEmpty
-                            ? (LatLng value) async {
-                          dialogTakeImage.dialogBuilder(
-                              context, value, listaMarkers, setState);
-                        }
-                            : null,
-                        myLocationEnabled: true,
-                        initialCameraPosition: CameraPosition(
-                          target: posizioneGeo,
-                          zoom: 11.0,
-                        ),
-                        markers: listaMarkers
-                            .map((customMarker) => customMarker.marker)
-                            .toSet(),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: listaMarkers.length,
-                    itemBuilder: (context, index) {
-
-                      return Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          color: Colors.blue.shade50, // Colore card chiaro
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: ListTile(
-                              trailing  : GestureDetector(
-                                onTap: () {
-                                  DialogImage().dialogBuilder(context, listaMarkers[index].imageUrl);
-                                },
-                                child: Image.network(listaMarkers[index].imageUrl.first),
-                              ),
-                              title: Text(listaMarkers[index].titolo),
-                              onTap: () {
-                                mapController.animateCamera(
-                                  CameraUpdate.newCameraPosition(
-                                    CameraPosition(
-                                      target: listaMarkers[index].marker.position,
-                                      zoom: 11,
-                                    ),
-                                  ),
-                                );
-                              },
-                              subtitle: Text(listaMarkers[index].descrizione),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            );
           } else {
-            return Center(child: Text('Unexpected error'));
+            // Se non è possibile ottenere la geolocalizzazione, uso Palermo come fallback
+            posizioneGeo = const LatLng(38.116669, 13.366667);
           }
+
+          // La logica della UI è unificata qui, indipendentemente dall'esito della geolocalizzazione
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey.shade200,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      onTap: loginProvider.token.isNotEmpty
+                          ? (LatLng value) async {
+                        dialogTakeImage.dialogBuilder(
+                            context, value, listaMarkers, setState);
+                      }
+                          : null,
+                      myLocationEnabled: true,
+                      initialCameraPosition: CameraPosition(
+                        target: posizioneGeo,
+                        zoom: 11.0,
+                      ),
+                      markers: listaMarkers
+                          .map((customMarker) => customMarker.marker)
+                          .toSet(),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: listaMarkers.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        color: Colors.blue.shade50, // Colore card chiaro
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: ListTile(
+                            trailing: GestureDetector(
+                              onTap: () {
+                                DialogImage().dialogBuilder(
+                                    context, listaMarkers[index].imageUrl);
+                              },
+                              child: Image.network(
+                                  listaMarkers[index].imageUrl.first),
+                            ),
+                            title: Text(listaMarkers[index].titolo),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.newCameraPosition(
+                                  CameraPosition(
+                                    target: listaMarkers[index].marker.position,
+                                    zoom: 11,
+                                  ),
+                                ),
+                              );
+                            },
+                            subtitle: Text(listaMarkers[index].descrizione),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
         },
       ),
     );
